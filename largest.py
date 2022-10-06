917f998c2b9b3b11b18b5e64f20b4cc3a5b72ef4
jerryjohnsonk <jerryjohnsonk@gmail.com>
2022-10-06 19:52:16 +0530

    Largest num add

---
+++ largest.py
@@ -0,0 +1,14 @@
+def Largenum():
+    print()
+
+print('Enter the number of elements:')
+n=int(input())
+List1=[]
+print("Enter the elements:")
+for i in range(0,n):
+     List1.append(input())
+print("Entered array is:")
+for i in range(0,n):
+    (
+        print(List1[i])
+    )
