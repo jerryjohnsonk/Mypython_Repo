398a75ccfb8fff547173392db0f899cf6c2078ee
jerryjohnsonk <jerryjohnsonk@gmail.com>
2022-10-06 20:26:53 +0530

    Checking function capability
    List as a function argument

--- largest.py
+++ largest.py
@@ -1,5 +1,6 @@
-def Largenum():
-    print()
+def Largenum(L1):
+    print("Code checking")
+    return 5
 
 print('Enter the number of elements:')
 n=int(input())
@@ -12,3 +13,5 @@ for i in range(0,n):
     (
         print(List1[i])
     )
+lnum = Largenum(List1)
+print(lnum)
